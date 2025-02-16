a6aac3bd7ef791ad581091a5655be09f1462ca56
DevBhargab <anweshabhargab2024@outlook.com>
2025-02-16 13:11:19 +0530

    branch: testing my learning

--- bhargab.c
+++ bhargab.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
 int main(){
-    printf("hello from bhargab")
-    return 0;
+    int day ;
+    scanf("%d",&day);
+    switch(day){
+        case 1:
+        printf("suryaday");
+        break;
+        case 2:
+        printf("monyday");
+        break;
+        case 3:
+        printf("tusiday");
+        break;
+        case 4:
+        printf("wednessday");
+        break;
+        case 5:
+        printf("thirdday");
+        break;
+        case 6:
+        printf("fryday");
+        break;
+        case 7:
+        printf("saniday");
+        break;
+    }
+         
+return 0 ;
 }
